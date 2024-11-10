5a78c40255abab8a295a19a61a0eaa39ebc3a421
elcastigador21 <elisabt123@gmail.com>
2024-11-10 21:35:29 +0100

    Navegabilidad implementada entre main activity y selectWorld :sparkles: Cambios en la distribucion visual de MainActivity :zap: Animacion implementada en MainActivity :sparkles:

--- app/src/main/java/com/grupo5/prueba_2/MainActivity.kt
+++ app/src/main/java/com/grupo5/prueba_2/MainActivity.kt
@@ -3,8 +3,12 @@ package com.grupo5.prueba_2
 import android.content.Intent
 import android.content.SharedPreferences
 import android.os.Bundle
+import android.view.View
+import android.view.animation.AlphaAnimation
 import android.widget.Button
 import android.widget.RadioButton
+import android.widget.ScrollView
+import android.widget.TextView
 import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
 import androidx.constraintlayout.widget.ConstraintLayout
@@ -16,78 +20,30 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        //make custom directs user to the activity where user can input his own dimensions of the board
-        val makeCustom : Button = findViewById(R.id.Laboratorio)
-        makeCustom.setOnClickListener{
+
+
+        val laboratorio : Button = findViewById(R.id.Laboratorio)
+        laboratorio.setOnClickListener{
             val intent = Intent(this@MainActivity, MakeCustomBoard::class.java)
             startActivity(intent)
         }
 
-        val mapa : ConstraintLayout = findViewById(R.id.mapa)
-        mapa.setOnClickListener{
-            val intent = Intent(this@MainActivity, gameWon::class.java)
+        val pantallaInicial : ConstraintLayout = findViewById(R.id.pantallaInicial)
+        pantallaInicial.setOnClickListener{
+            val intent = Intent(this, SelectWorld::class.java)
             startActivity(intent)
         }
-        /*var level=-1
-        //self indicates the board to be created itself by the system as per the difficulty chosen by user
-        val boardType= "self"
-        val easy : RadioButton = findViewById(R.id.easy)
-        easy.setOnClickListener{
-            level=0
+
+        val tocaParaJugar : TextView = findViewById(R.id.toca_para_jugar)
+        //Animacion de parpadeo del texto
+        val animacionParpadeo = AlphaAnimation(0.0f, 1.0f).apply{
+            duration = 500
+            startOffset = 20
+            repeatMode = AlphaAnimation.REVERSE
+            repeatCount = AlphaAnimation.INFINITE
         }
-        val medium : RadioButton = findViewById(R.id.medium)
-        medium.setOnClickListener{
-            level=1
-        }
-        val difficult : RadioButton = findViewById(R.id.difficult)
-        difficult.setOnClickListener{
-            level=2
-        }
-        // start game will take the user to the game screen but will show an alert dialog ton the user if no difficulty is chosen,instructing the user to do so
-        val startGame : Button = findViewById(R.id.Mapa)
-        startGame.setOnClickListener {
-            if(level==-1) {
-                val builder= AlertDialog.Builder(this)
-                with(builder)
-                {
-                    setTitle(getString(R.string.difficulty_message))
-                    setMessage(getString(R.string.select_difficulty_message))
-                    setCancelable(false)
-                    setPositiveButton("Ok"
-                    ) { dialog, which -> }
-                }
-                val dialog= builder.create()
-                dialog.show()
-
-            } else {
-                val intent= Intent(this, MinesweeperGame::class.java).apply {
-                    putExtra("Level",level)
-                    putExtra("boardType", boardType)
-                }
-                startActivity(intent)
-            }
-        }
-        // on clicking stats user will be directed to stats page where all the stats regarding game are visible
-        val stats : Button = findViewById(R.id.stats)
-        stats.setOnClickListener {
-    // these shared preferences are directed to main activity by the mine sweeper game activity
-            sharedPreferences=
-                this.getSharedPreferences("Stats",Context.MODE_PRIVATE)
-
-            val intent = Intent(this, GameStats::class.java).apply {
-                with(sharedPreferences) {
-                    putExtra("bestTime",  getInt("best",0))
-                    putExtra("lastTime",getInt("last",0))
-                    putExtra("played", getInt("total",0))
-                    putExtra("won",getInt("won",0))
-                    putExtra("lost",getInt("lost",0))
-                }
-
-            }
-
-            startActivity(intent)
-        }*/
-
+        //Ver si se puede poner una lambda
+        tocaParaJugar.startAnimation(animacionParpadeo)
 
     }
 }
