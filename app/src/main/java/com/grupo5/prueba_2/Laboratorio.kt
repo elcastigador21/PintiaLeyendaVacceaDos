96fddb53b087cbadd9178517580e6a044a55dd5b
Daniel Martinez Alvarez <daniel.martinez.alvarez@estudiantes.uva.es>
2024-12-10 12:09:36 +0100

    creacion de activity_reliquias.cml y arreglo de fallos referente a un nombre de imagen

--- app/src/main/java/com/grupo5/prueba_2/Laboratorio.kt
+++ app/src/main/java/com/grupo5/prueba_2/Laboratorio.kt
@@ -31,13 +31,26 @@ class Laboratorio : AppCompatActivity(){
         setContentView(R.layout.activity_laboratorio)
 
         val reliquias : ListView = findViewById(R.id.reliquias)
-
+        val nombres = mutableListOf<String>()
+        val descripciones = mutableListOf<String>()
 
         // Leer el JSON desde filesDir y convertirlo en una lista
         val lista = leerJson(outputFileName)
 
+        for(elemento in lista){
+            val partes = elemento.split("-")
+            if(partes.size==2) {
+                nombres.add(partes[0])
+                descripciones.add(partes[1])
+            }
+            else{
+                nombres.add(partes[0])
+                descripciones.add("???")
+            }
+        }
+
         // Crear el adaptador y asociarlo con el ListView
-        val adapter = ArrayAdapter(this, R.layout.elemento_lista, lista)
+        val adapter = ArrayAdapter(this, R.layout.elemento_lista, nombres)
         reliquias.adapter = adapter
 
         // Notificar al adaptador que los datos han cambiado
@@ -93,10 +106,11 @@ class Laboratorio : AppCompatActivity(){
         for (i in 0 until jsonArray.length()) {
             val objeto = jsonArray.getJSONObject(i)
             val nombre = if (objeto.getBoolean("descubierta")) {
-                objeto.getString("nombre")
+                objeto.getString("nombre")+"-"+objeto.getString("descripcion")
             } else {
                 "???"
             }
+            print(nombre)
             palabras.add(nombre)
         }
         return palabras
