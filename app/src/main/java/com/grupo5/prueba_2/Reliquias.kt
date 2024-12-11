68db1ee9d8183c563eb3db8e624b0220143e70eb
Daniel Martinez Alvarez <daniel.martinez.alvarez@estudiantes.uva.es>
2024-12-11 20:22:04 +0100

    resuelto fallo

--- app/src/main/java/com/grupo5/prueba_2/Reliquias.kt
+++ app/src/main/java/com/grupo5/prueba_2/Reliquias.kt
@@ -4,6 +4,7 @@ import android.content.Intent
 import android.content.SharedPreferences
 import android.net.Uri
 import android.os.Bundle
+import android.text.method.ScrollingMovementMethod
 import android.view.animation.AlphaAnimation
 import android.widget.ArrayAdapter
 import android.widget.Button
@@ -35,6 +36,7 @@ class Reliquias : AppCompatActivity(){
         val descripcion:TextView = findViewById(R.id.Descripcion)
         val imagen:TextView = findViewById(R.id.Imagen)
 
+        descripcion.movementMethod=ScrollingMovementMethod()
         val imagenesPorNombre = mapOf(
             "Broche de perro" to R.drawable.broche_de_perro,
             "Pomo de puñal" to R.drawable.pomo_de_punal,
@@ -50,7 +52,7 @@ class Reliquias : AppCompatActivity(){
 
         if (nombres != null && descripciones != null) {
             nombre.text = nombres.get(posicion)
-            descripcion.text = "El zoomorfo vacceo es una representación simbólica y artística de un animal que forma parte de la cultura de los vacceos, un antiguo pueblo prerromano que habitó la meseta norte de la península ibérica en lo que hoy corresponde a las provincias de Valladolid, Palencia, Zamora, Salamanca y Segovia, entre otros lugares. Estas figuras se realizaban en distintos materiales como piedra, bronce o cerámica y podían tener diversos tamaños, desde pequeñas piezas portátiles hasta grandes esculturas. Los zoomorfos vacceos estaban cargados de significados rituales y espirituales, ya que los vacceos eran una sociedad agraria profundamente conectada con la naturaleza y sus ciclos, atribuyendo a los animales poderes simbólicos y mágicos. Por ejemplo, un toro representado en una escultura podría simbolizar fuerza, fertilidad y protección, reflejando la importancia de este animal en su economía basada en la agricultura y la ganadería. Las formas estilizadas o geométricas de estos animales denotan una clara influencia de las tradiciones artísticas celtas, combinando simplicidad y simbolismo en sus diseños. Los contornos pueden ser toscos o refinados dependiendo de la técnica y el propósito de la obra, pero siempre destacan por resaltar las características esenciales del animal, como los cuernos en el caso de un toro o las garras en el caso de un ave rapaz. Además, estas figuras no solo eran ornamentales, sino que se les atribuía un papel activo en los rituales, sirviendo como amuletos, ofrendas votivas o elementos protectores en tumbas y hogares. En algunos casos, los zoomorfos vacceos podían estar asociados a divinidades o a prácticas de culto relacionadas con la fertilidad de la tierra, el éxito en la caza o la protección del clan. Su diseño no se limitaba a la representación literal, ya que a menudo se combinaban elementos de diferentes animales para crear figuras híbridas que acentuaban su carácter místico, fusionando por ejemplo la fuerza de un toro con la agilidad de un lobo. El hecho de que estos objetos hayan perdurado hasta nuestros días demuestra no solo su durabilidad material, sino también la importancia que tuvieron en la vida cotidiana y espiritual de este pueblo, proporcionando valiosas pistas sobre sus creencias y su visión del mundo."
+            descripcion.text=descripciones[posicion]
             imagen.setBackgroundResource(imagenesPorNombre[nombres.get(posicion)] ?: R.drawable.bloqueado)
             imagen.text=""
 
